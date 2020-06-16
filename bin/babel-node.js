43ffc37f547f96346dc9d65d075f986a0cf5ac5f
ChenShuzhou <527533739@qq.com>
2020-06-16 18:29:08 +0800

    update

--- bin/babel-node.js
+++ bin/babel-node.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
-const spawn = require("cross-spawn");
-var path = require("path");
+var shell = require('shelljs');
+var path = require('path');
 let file;
 const argv = process.argv;
-//添加node-babel默认命令
-if (!argv.includes["node-babel"]) {
-  argv.splice(2, 0, "node-babel");
+if (!argv.includes('node-babel') && !argv.includes('init')) {
+  argv.splice(2, 0, 'node-babel');
 }
 
-const action = () => {
-  console.log(file);
-  spawn.sync("npx", ["babel-node", file], {
-    stdio: "inherit",
+const init = () => {
+  const cmd = 'cnpm i @babel/node @babel/core @babel/preset-env -D';
+  shell.exec(cmd, {}, (...arg) => {
+    console.log(...arg);
   });
 };
-var program = require("commander");
-program
-  .version(require("../package.json").version)
-  .usage("-f <name>.js 全局g-babel-node");
 
-program.option("-f, --file <string>", "relative path file");
-program.parse(argv);
-if (program.file !== undefined)
-  file = path.resolve(process.cwd(), program.file);
+const action = dir => {
+  file = path.resolve(process.cwd(), dir);
+  const cmd = `npx babel-node ${file} --presets @babel/env`;
+  shell.exec(cmd, {}, (...arg) => {
+    console.log(...arg);
+  });
+};
+var program = require('commander');
+program.command('node-babel <dir>').action(action);
+program.command('init').description('init').action(init);
 
-program.command("node-babel").description("node-babel").action(action);
 program.parse(argv);
